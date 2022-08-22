833f39319dbb71b7b1f6cc5e041d898cc5c2a554
Ade Mustofa <ademustofa286@gmail.com>
2022-08-22 23:27:11 +0700

    add testing testing operation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 test('test 2 nilai perkalian', () => {
-
+  const val = 2 * 2
+  expect(val).toBe(4);
 })
 
 test('test 2 nilai penambahan', () => {
-
+  const val = 2 + 3
+  expect(val).toBe(5);
 })
 
 test('test 2 nilai pengurangan', () => {
-
+  const val = 2 * 10
+  expect(val).toBe(20);
 })
 
 test('test 2 nilai pembagian', () => {
-
+  const val = 10 / 2
+  expect(val).toBe(5);
 })
 
 test('test 3 nilai perkalian', () => {
-
+  const val = 10 * 2 * 2
+  expect(val).toBe(40);
 })
 
 test('test 3 nilai penambahan', () => {
-
+  const val = 10 + 2 + 2
+  expect(val).toBe(14);
 })
 
 test('test 3 nilai pengurangan', () => {
-
+  const val = 10 - 2 - 2
+  expect(val).toBe(6);
 })
 
 test('test 3 nilai pembagian', () => {
-
+  const val = 10 / 2 / 5
+  expect(val).toBe(1);
 })
 
 
